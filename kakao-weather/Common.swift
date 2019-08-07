b7c8fb0562ee04acc9d06def0b0269d391b4065d
passionkmk <passion.kmk@gmail.com>
2019-08-07 22:30:06 +0900

    feat(common): Add Common

---
+++ kakao-weather/Common.swift
@@ -0,0 +1,25 @@
+//
+//  Common.swift
+//  kakao-weather
+//
+//  Created by 김민구 on 07/08/2019.
+//  Copyright © 2019 pkmk. All rights reserved.
+//
+
+import Foundation
+
+typealias RouterResponse = (data: Data, code: Int)
+typealias ApiResult = (Data?)
+
+enum Host {
+    static let base: String = "weather-ydn-yql.media.yahoo.com"
+    
+    static let addId: String = "h80UJe3c"
+    static let clientId: String = "dj0yJmk9Rk51cW4zZ1hWeGEwJmQ9WVdrOWFEZ3dWVXBsTTJNbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWE2"
+    static let clientSecret: String = "94a7f4f721907b9bbb3a3d95950246a2ee6cdfa1"
+    
+    static let header: [String: String] = [
+        "Authorization": "",
+        "Content-Type": "application/json"
+    ]
+}
